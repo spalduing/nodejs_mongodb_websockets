851173aee6b2acc6e3efff7eb43eda5db0d78565
mdiazc <spalduing12@gmail.com>
2022-06-02 14:59:36 -0500

    Added: Specifict routes / endpoints using the express router

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', function (req, res) {
-  res.send('Hello there!');
+const router = express.Router();
+// app.use('/', function (req, res) {
+//   res.send('Hello there!');
+// });
+
+app.use(router);
+
+router.get('/', function (req, res) {
+  res.send('Hello from get');
+});
+router.post('/', function (req, res) {
+  res.send('Hello from post');
+});
+router.get('/message', function (req, res) {
+  res.send('List of messages');
+});
+router.post('/message', function (req, res) {
+  res.send('Message added');
 });
 
 app.listen(3000);
