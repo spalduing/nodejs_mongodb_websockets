bd83261a14d53b4c1cc154357be1f2f7d4eee47b
mdiazc <spalduing12@gmail.com>
2022-06-04 22:45:55 -0500

    Added: Socket.io basic example

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const db = require('./db');
 const routes = require('./network/routes');
 const config = require('./network/config');
 
-const server = express();
-
 const uri = `mongodb+srv://${config.user}:${config.pasword}@cluster0.nxuer.mongodb.net/${config.db_name}`;
 db(uri);
 
-routes(server);
+const app = express();
+routes(app);
 
-server.listen(3000);
+const server = require('http').Server(app);
 
-console.log('The application is lissening at http://localhost:3000 port');
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('New client get conected');
+  socket.emit('message', 'Welcome!');
+});
+
+setInterval(() => {
+  io.emit('message', 'Hi, Im notifying you guys');
+}, 3000);
+server.listen(3000, () => {
+  console.log('The application is lissening at http://localhost:3000 port');
+});
